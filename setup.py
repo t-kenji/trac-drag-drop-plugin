8d282c0190cd1c4e2b11e46b7fb9f4180cbd3bb5
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-06-14 07:02:26 +0000

    * For 0.11: Bump up the version to 0.11.0.3  * For 0.12: Port to Trac 0.12 and L10N
    git-svn-id: https://trac-hacks.org/svn/tracdragdropplugin/0.12@8116 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+extra = {}
+
+try:
+    import babel
+    extra['message_extractors'] = {
+        'tracdragdrop': [
+            ('**.py', 'python', None),
+            ('**.html', 'genshi', None),
+        ],
+    }
+    from trac.util.dist import get_l10n_js_cmdclass
+    extra['cmdclass'] = get_l10n_js_cmdclass()
+except ImportError:
+    pass
+
+setup(
+    name = 'TracDragDrop',
+    version = '0.12.0.3',
+    description = 'Add drag-and-drop attachments feature to Trac',
+    license = 'BSD',
+    url = 'http://trac-hacks.org/wiki/TracDragDropPlugin',
+    author = 'Jun Omae',
+    author_email = 'jun66j5@gmail.com',
+    packages = find_packages(exclude=['*.tests*']),
+    package_data = {
+        'tracdragdrop' : [
+            'htdocs/*.js', 'htdocs/*.css', 'templates/*.html',
+            'htdocs/messages/*.js',
+        ],
+    },
+    entry_points = {
+        'trac.plugins': [
+            'tracdragdrop = tracdragdrop.web_ui',
+        ],
+    },
+    **extra
+)
+
