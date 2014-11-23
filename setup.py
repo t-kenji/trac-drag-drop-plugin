7b300fa9320831d37b754545c5b72b10f3b1874f
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-11-23 13:21:27 +0000

    TracDragDropPlugin: call `setuptools.setup()` only if run `setup.py` as a script
    git-svn-id: https://trac-hacks.org/svn/tracdragdropplugin/0.12@14282 7322e99d-02ea-0310-aa39-e9a107903beb

--- setup.py
+++ setup.py
@@ -3,41 +3,45 @@
 
 from setuptools import setup, find_packages
 
-extra = {}
+def main():
+    extra = {}
 
-try:
-    import babel
-    extra['message_extractors'] = {
-        'tracdragdrop': [
-            ('**.py', 'python', None),
-            ('**.html', 'genshi', None),
-        ],
-    }
-    from trac.util.dist import get_l10n_js_cmdclass
-    extra['cmdclass'] = get_l10n_js_cmdclass()
-except ImportError:
-    pass
+    try:
+        import babel
+        extra['message_extractors'] = {
+            'tracdragdrop': [
+                ('**.py', 'python', None),
+                ('**.html', 'genshi', None),
+            ],
+        }
+        from trac.util.dist import get_l10n_js_cmdclass
+        extra['cmdclass'] = get_l10n_js_cmdclass()
+    except ImportError:
+        pass
 
-setup(
-    name = 'TracDragDrop',
-    version = '0.12.0.12',
-    description = 'Add drag-and-drop attachments feature to Trac',
-    license = 'BSD', # the same as Trac
-    url = 'http://trac-hacks.org/wiki/TracDragDropPlugin',
-    author = 'Jun Omae',
-    author_email = 'jun66j5@gmail.com',
-    install_requires = ['Trac >= 0.12'],
-    packages = find_packages(exclude=['*.tests*']),
-    package_data = {
-        'tracdragdrop' : [
-            'htdocs/*.js', 'htdocs/*.css', 'htdocs/*.gif', 'templates/*.html',
-            'htdocs/messages/*.js',
-            'locale/*/LC_MESSAGES/tracdragdrop.mo',
-        ],
-    },
-    entry_points = {
-        'trac.plugins': [
-            'tracdragdrop.web_ui = tracdragdrop.web_ui',
-        ],
-    },
-    **extra)
+    setup(
+        name = 'TracDragDrop',
+        version = '0.12.0.12',
+        description = 'Add drag-and-drop attachments feature to Trac',
+        license = 'BSD', # the same as Trac
+        url = 'http://trac-hacks.org/wiki/TracDragDropPlugin',
+        author = 'Jun Omae',
+        author_email = 'jun66j5@gmail.com',
+        install_requires = ['Trac >= 0.12'],
+        packages = find_packages(exclude=['*.tests*']),
+        package_data = {
+            'tracdragdrop': [
+                'htdocs/*.js', 'htdocs/*.css', 'htdocs/*.gif',
+                'templates/*.html', 'htdocs/messages/*.js',
+                'locale/*/LC_MESSAGES/tracdragdrop.mo',
+            ],
+        },
+        entry_points = {
+            'trac.plugins': [
+                'tracdragdrop.web_ui = tracdragdrop.web_ui',
+            ],
+        },
+        **extra)
+
+if __name__ == '__main__':
+    main()
