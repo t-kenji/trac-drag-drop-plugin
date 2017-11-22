683272784491646b04c60f61a468f660e4df55df
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-11-22 03:37:54 +0000

    TracDragDropPlugin: bump up the version to 0.12.0.14
    git-svn-id: https://trac-hacks.org/svn/tracdragdropplugin/0.12@16999 7322e99d-02ea-0310-aa39-e9a107903beb

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def main():
 
     setup(
         name = 'TracDragDrop',
-        version = '0.12.0.13',
+        version = '0.12.0.14',
         description = 'Add drag-and-drop attachments feature to Trac',
         license = 'BSD', # the same as Trac
         url = 'http://trac-hacks.org/wiki/TracDragDropPlugin',
